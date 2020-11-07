3d1dee4d9e1c07d7e0f5376b5e9597d75c2f627f
TANTANMOY <tanmoymandal457@gmail.com>
2020-11-07 17:56:28 +0530

    added loading

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: WebViewClass()
+        )
+    );
+  }
+}
+
+class WebViewClass extends StatefulWidget {
+
+  WebViewState createState() => WebViewState();
+
+}
+
+class WebViewState extends State<WebViewClass>{
+
+  num position = 1 ;
+
+  final key = UniqueKey();
+
+  doneLoading(String A) {
+    setState(() {
+      position = 0;
+    });
+  }
+
+  startLoading(String A){
+    setState(() {
+      position = 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text('Show ProgressBar While Loading Webview')),
+        body: IndexedStack(
+            index: position,
+            children: <Widget>[
+
+              WebView(
+                initialUrl: 'https://www.google.com',
+                javascriptMode: JavascriptMode.unrestricted,
+                key: key ,
+                onPageFinished: doneLoading,
+                onPageStarted: startLoading,
+              ),
+
+              Container(
+                color: Colors.white,
+                child: Center(
+                    child: CircularProgressIndicator()),
+              ),
+
+            ])
+    );
+  }
+}
